1d2f114780861f401d4f70b2c5aca4b4b986bf7f
Chad Cote <chadcote58@gmail.com>
2024-07-18 15:54:56 -0400

    Changed logic for retrieve_ship to handle expand query param

--- views/ship_view.py
+++ views/ship_view.py
@@ -108,25 +108,57 @@ def list_ships(url):
     return serialized_ships
 
 
-def retrieve_ship(pk):
+def retrieve_ship(pk, url):
     # Open a connection to the database
     with sqlite3.connect("./shipping.db") as conn:
         conn.row_factory = sqlite3.Row
         db_cursor = conn.cursor()
 
-        # Write the SQL query to get the information you want
-        db_cursor.execute("""
-        SELECT
-            s.id,
-            s.name,
-            s.hauler_id
-        FROM Ship s
-        WHERE s.id = ?
-        """, (pk,))
-        query_results = db_cursor.fetchone()
+        if '_expand' in url['query_params']:
+            # Write the SQL query to get the expanded information
+            db_cursor.execute("""
+            SELECT
+                s.id,
+                s.name,
+                s.hauler_id,
+                h.id AS haulerId,
+                h.name AS haulerName,
+                h.dock_id
+            FROM Ship s
+            JOIN Hauler h
+                ON h.id = s.hauler_id
+            WHERE s.id = ?
+            """, (pk,))
+            row = db_cursor.fetchone()
+            if row:
+                hauler = {
+                    "id": row['haulerId'],
+                    "name": row['haulerName'],
+                    "dock_id": row["dock_id"]
+                }
+                ship = {
+                    "id": row['id'],
+                    "name": row['name'],
+                    "hauler_id": row["hauler_id"],
+                    "hauler": hauler
+                }
+            else:
+                ship = None
+        else:
+            # Write the SQL query to get the basic information
+            db_cursor.execute("""
+            SELECT
+                s.id,
+                s.name,
+                s.hauler_id
+            FROM Ship s
+            WHERE s.id = ?
+            """, (pk,))
+            row = db_cursor.fetchone()
+            ship = dict(row) if row else None
 
-        # Serialize Python list to JSON encoded string
-        dictionary_version_of_object = dict(query_results)
-        serialized_ship = json.dumps(dictionary_version_of_object)
+        # Serialize Python dictionary to JSON encoded string
+        serialized_ship = json.dumps(ship) if ship else json.dumps({})
 
     return serialized_ship
+
